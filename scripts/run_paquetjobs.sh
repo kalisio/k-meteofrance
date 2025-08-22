c1116888b9e44412e618e901256f89ad26c93722
valentin.lepaul <valentin-alexy.lepaul@epitech.eu>
2025-08-22 18:46:59 +0400

    wip : Provide a run_paquetjobs.sh script #3

--- scripts/run_paquetjobs.sh
+++ scripts/run_paquetjobs.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
+export FREQUENCY="horaire"
+export LATENCY="0"
+export TTL="604800"
 krawler ./jobfile.paquetobs-observations.js
+export DEPARTMENTS="11,09"
+export STATIONS="1014002"
 krawler ./jobfile.paquetobs-stations.js
