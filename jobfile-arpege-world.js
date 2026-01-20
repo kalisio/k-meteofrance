4efdd95b0259424deff224d6d66e2a375c454cb4
tristan-greffe <tristan.greffe@hotmail.com>
2026-01-20 11:05:13 +0100

    fix: align forecastTimes with ARPEGE technical documentation

--- jobfile-arpege-world.js
+++ jobfile-arpege-world.js
@@ -8,5 +8,5 @@ export default createJob({
   format: 'grib2',
   runTimes: ['00:00:00', '06:00:00', '12:00:00', '18:00:00'],
   packages: ['HP1', 'HP2', 'IP1', 'IP2', 'IP3', 'IP4', 'SP1', 'SP2'],
-  forecastTimes: ['000H012H', '013H024H', '025H036H', '037H048H', '049H060H', '061H072H', '073H084H', '073H084H', '085H096H', '097H102H']
+  forecastTimes: ['000H024H', '025H048H', '049H072H', '073H102H' ]
 })
