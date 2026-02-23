381d59f57d00bf41c66ed50fd00cfc29674683c1
Tristan <tristan.greffe@hotmail.com>
2026-02-23 19:18:26 +0100

    wip: Implement GRIB2 to Zarr file transformation job #6

---
+++ jobfile-grib2-to-zarr.js
@@ -0,0 +1,90 @@
+import _ from 'lodash'
+import winston from 'winston'
+import { hooks } from  '@kalisio/krawler'
+import fs from 'fs'
+import path from 'path'
+
+// Job configuration
+const outputDir = './output'
+const workersLimit = process.env.WORKERS_LIMIT ? Number(process.env.WORKERS_LIMIT) : 2
+
+// Register generateTasks hook
+const generateTasks = () => {
+	return function (hook) {
+		const directoryEntries = fs.readdirSync(outputDir, { withFileTypes: true })
+		const tasks = []
+		for (const entry of directoryEntries) {
+			const folderName = entry.name
+			const folderFullPath = path.join(outputDir, folderName)
+			// List the contents of the folder
+			const filesInFolder = fs.readdirSync(folderFullPath)
+			// Check if there is a .grib2 file
+      const hasGrib2 = filesInFolder.some(file => file.endsWith('.grib2'))
+			// Check if there is a DONE.txt file
+      const hasDoneFile = filesInFolder.includes('DONE.txt')
+			if (hasGrib2 && !hasDoneFile) tasks.push({ id: folderFullPath })
+		}	
+		hook.data.tasks = tasks
+		return hook
+	}
+}
+hooks.registerHook('generateTasks', generateTasks)
+
+export default {
+  id: 'grib2-to-zarr',
+  store: 'fs',
+  options: {
+    workersLimit,
+    faultTolerant: true,
+  },
+  tasks: [{
+		id: '<%= taskId %>',
+		type: 'noop',
+    store: 'fs'
+  }],
+  hooks: {
+    tasks: {
+			before: {
+				log: (logger, item) => logger.verbose(`Creating task for ${item.id}`),
+			},
+      after: {
+        grib2ToZarr: {
+          hook: 'runCommand',
+					// TODO 
+					// convert grib2 to zaar only files with a .grib2 extension and add a Done.txt file if all files are present ( can be done via an env)
+          command: 'echo TODO',
+					stdout: true,
+        }
+      }
+    },
+    jobs: {
+      before: {
+				createStores: {
+					id: 'fs',
+					type: 'fs',
+					options: { path: outputDir }
+				},
+				createLogger: {
+					loggerPath: 'taskTemplate.logger',
+					Console: {
+						format: winston.format.printf(log => winston.format.colorize().colorize(log.level, `${log.level}: ${log.message}`)),
+						level: 'verbose'
+					}
+				},
+				generateTasks:  {}
+      },
+      after: {
+				removeStores: ['fs'],
+				removeLogger: {
+					loggerPath: 'taskTemplate.logger'
+				}
+      },
+      error: {
+				removeStores: ['fs'],
+				removeLogger: {
+					loggerPath: 'taskTemplate.logger'
+				}
+			}
+    }
+  }
+}
